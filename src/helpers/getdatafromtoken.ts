e1548d05e4db90e5b20b92774638cab8f5d94bba
Mahesh Babu <bee_2024016@iiitm.ac.in>
2026-07-11 19:43:00 +0530

    added

--- src/helpers/getdatafromtoken.ts
+++ src/helpers/getdatafromtoken.ts
@@ -1,15 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { NextRequest } from "next/server";
 
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || "";
 
+        const decodedToken = jwt.verify(
+            token,
+            process.env.TOKEN_SECRET!
+        ) as JwtPayload;
 
-export const getdatafromtoken =(request :NextRequest)=>{
-    try{
-        const token =request.cookies.get("token")?.value|| ''
-       const decodedtoken = jwt.verify(token,process.env.TOKEN_SECRET!)
-       return decodedtoken.id
+        return decodedToken.id;
+    } catch (error: any) {
+        throw new Error(error.message);
     }
-    catch(error:any){
-        throw new Error(error.message)
-    }
-}
+};
